8a5c3f81cae0915080d313189fac27cbcff39775
12345elakkiya <47380966+12345elakkiya@users.noreply.github.com>
2019-02-27 22:25:49 +0530

    Rename large.java to largest.java

---
+++ largest.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class large
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num1 = 3, num2 = 4, num3 = 5;
+
+      if( num1 >= num2 && num1 >= num3)
+          System.out.println("3");
+
+      else if (num2 >= num1 && num2 >= num3)
+          System.out.println("4");
+
+      else
+          System.out.println("5");
+  }
+}
+	
